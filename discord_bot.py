7aa11f37342a65384ddc787e3eafbf3788364325
Lucas <lucasbaumann5@icloud.com>
2018-07-12 17:19:25 +0200

    first test

---
+++ discord_bot.py
@@ -0,0 +1,27 @@
+import discord
+
+TOKEN = 'NDY1Mjg3MTU3ODMwMTIzNjA4.DiLXeQ.2OI81W9s3Clpz9dvpytzbNwhIi8'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        hello = ':wave: Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, hello)
+    elif message.content.startswith('!help'):
+        help = '"!hello": lässt den bot antworten'.format(message)
+        await client.send_message(message.channel, help)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
